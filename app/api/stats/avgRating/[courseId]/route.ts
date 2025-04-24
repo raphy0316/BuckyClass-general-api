2aa8d0cba8095878d95641ec716296ca6d028e56
Cheryn Kim <ckim0202@gmail.com>
2025-04-24 18:27:20 -0500

    edited avgRating

--- app/api/stats/avgRating/[courseId]/route.ts
+++ app/api/stats/avgRating/[courseId]/route.ts
@@ -1,12 +1,14 @@
 import { getAverageRatingByCourse } from "@/app/services/postgreService";
 
-export async function GET(request: Request, context: { params: { courseId: string } }) {
+export async function GET(
+  request: Request,
+  { params }: { params: { courseId: string } }
+) {
   try {
-    const courseId = context.params.courseId;
-    const result = await getAverageRatingByCourse(courseId);
+    const result = await getAverageRatingByCourse(params.courseId);
     return Response.json(result, { status: 200 });
   } catch (error) {
-      console.error("Failed to  average rating", error);
-      return Response.json({ error: "Failed to fetch average rating" }, { status: 500 });
+    console.error("Failed to fetch average rating", error);
+    return Response.json({ error: "Failed to fetch average rating" }, { status: 500 });
   }
 }
